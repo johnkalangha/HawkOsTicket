45e497b3efff100496119a2981beb06e46ce7f75
Fernando Isidro <luffynando@gmail.com>
2018-12-18 20:28:30 -0600

    Initial commit

---
+++ include/class.themes.php
@@ -0,0 +1,16 @@
+<?php
+/*********************************************************************
+    class.themes.php
+
+    External adaptation themes for osTicket
+
+    RedHawk
+    Luffynando
+    Copyright by TeamRedhawk (www.teamredhawk.net)
+
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
+**********************************************************************/
+/**
+ * basic adaptation
+ */
