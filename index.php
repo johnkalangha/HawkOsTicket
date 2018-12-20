d7f6adb6df92a2876e101bfb11eb8781894ebe34
Fernando Isidro <luffynando@gmail.com>
2018-12-20 00:35:39 -0600

    Index client fixes and customizations :3

--- index.php
+++ index.php
@@ -23,17 +23,33 @@ require(CLIENTINC_DIR.'header.inc.php');
 <!-- Content Header (Page header) -->
     <!-- Main content -->
     <section class="content">
+    <?php
+    $sdshow=false;
+    $faqs = FAQ::getFeatured()->select_related('category')->limit(5);
+    $resources = Page::getActivePages()->filter(array('type'=>'other'));
+    if ($faqs->all() || $resources->all()) {
+        $sdshow = true;
+    } ?>
+    <div class="row">
+        <?php if($sdshow) {?>
+            <div class="col-md-8">
+        <?php } else { ?>
+            <div class="col-md-12">
+        <?php } ?>
         <div class="box box-default">
             <div class="box-body">
                 <?php
                 if ($cfg && $cfg->isKnowledgebaseEnabled()) { ?>
-                <div class="search-form">
-                    <form method="get" action="kb/faq.php">
-                    <input type="hidden" name="a" value="search"/>
-                    <input type="text" name="q" class="search" placeholder="<?php echo __('Search our knowledge base'); ?>"/>
-                    <button type="submit" class="green button"><?php echo __('Search'); ?></button>
-                    </form>
-                </div>
+                <form method="get" action="kb/faq.php">
+                    <div class="input-group">
+                        <input type="hidden" name="a" value="search"/>
+                        <input type="text" name="q" class="form-control" placeholder="<?php echo __('Search our knowledge base'); ?>">
+                        <span class="input-group-btn">
+                            <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                </form>
                 <?php
                 }
                 if($cfg && ($page = $cfg->getLandingPage()))
@@ -41,38 +57,105 @@ require(CLIENTINC_DIR.'header.inc.php');
                 else
                     echo  '<h1>'.__('Welcome to the Support Center').'</h1>';
                 ?>
-                <?php include CLIENTINC_DIR.'templates/sidebar.tmpl.php'; ?>
+                <?php
+                $BUTTONS = isset($BUTTONS) ? $BUTTONS : true;
+                ?>
+                <?php if ($BUTTONS) { ?>
+                <div class="row">
+                <?php
+                if ($cfg->getClientRegistrationMode() != 'disabled'
+                    || !$cfg->isClientLoginRequired()) { ?>
+                    <div class="col-lg-6 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3><?php echo __('Open');?></h3>
+
+                            <p><?php echo __('New Ticket'); ?></p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-paper-plane-o "></i>
+                        </div>
+                        <a href="open.php" class="small-box-footer">
+                        <?php
+                        echo __('Open a New Ticket');?> <i class="fa fa-arrow-circle-right"></i>
+                        </a>
+                        </div>
+                    </div>
+                <?php } ?>
+                    <div class="col-lg-6 col-xs-6">
+                    <!-- small box -->
+                        <div class="small-box bg-green">
+                            <div class="inner">
+                                <h3><?php echo __('Check');?></h3>
+
+                                <p><?php echo __('Ticket Status');?></p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-check-circle-o"></i>
+                            </div>
+                            <a href="view.php" class="small-box-footer">
+                            <?php
+                    echo __('Check Ticket Status');?> <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
             </div>
         </div>
+        </div>
+        <?php include CLIENTINC_DIR.'templates/sidebar.tmpl.php'; ?>
         <?php
         if($cfg && $cfg->isKnowledgebaseEnabled()){
             //FIXME: provide ability to feature or select random FAQs ??
             ?>
-            <?php
-            $cats = Category::getFeatured();
-            if ($cats->all()) { ?>
-                <h1><?php echo __('Featured Knowledge Base Articles'); ?></h1>
-            <?php
-            }
-            foreach ($cats as $C) { ?>
-            <div class="featured-category front-page">
-                <i class="icon-folder-open icon-2x"></i>
-                <div class="category-name">
-                    <?php echo $C->getName(); ?>
-                </div>
-                <?php foreach ($C->getTopArticles() as $F) { ?>
-                <div class="article-headline">
-                    <div class="article-title"><a href="<?php echo ROOT_PATH;
-                        ?>kb/faq.php?id=<?php echo $F->getId(); ?>"><?php
-                         echo $F->getQuestion(); ?></a></div>
-                    <div class="article-teaser"><?php echo $F->getTeaser(); ?></div>
-                </div>
+                <?php if($sdshow) {?>
+                <div class="col-md-8">
+                <?php } else { ?>
+                <div class="col-md-12">
                 <?php } ?>
-            </div>
-            <?php
-            }
-        }
+                    <div class="box box-info">
+                    <?php
+                    $cats = Category::getFeatured();
+                    if ($cats->all()) { ?>
+                        <div class="box-header with-border">
+                            <h3 class="box-title"><?php echo __('Featured Knowledge Base Articles'); ?></h3>
+                        </div>
+                        <?php
+                    } ?>
+                        <div class="box-body">
+                    <?php foreach ($cats as $C) { ?>
+                            <div class="box-group" id="accordion">
+                                <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+                                <div class="panel box box-primary">
+                                    <div class="box-header with-border">
+                                        <h4 class="box-title">
+                                        <a data-toggle="collapse" data-parent="#accordion" href="#<?php echo $C->getName(); ?>">
+                                        <i class="fa fa-folder-open-o"></i><?php echo $C->getName(); ?>
+                                        </a>
+                                        </h4>
+                                    </div>
+                                    <div id="<?php echo $C->getName(); ?>" class="panel-collapse collapse">
+                                        <div class="box-body">
+                                            <?php foreach ($C->getTopArticles() as $F) { ?>
+                                            <div class="article-headline">
+                                                <div class="article-title"><a href="<?php echo ROOT_PATH;
+                                                ?>kb/faq.php?id=<?php echo $F->getId(); ?>"><?php
+                                                echo $F->getQuestion(); ?></a></div>
+                                                <div class="article-teaser"><?php echo $F->getTeaser(); ?></div>
+                                            </div>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    <?php
+                    } ?>
+                        </div>
+        <?php }
         ?>
+    </div>
     </section>
     <!-- /.content -->
 <?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
