92c302df6ecff8a98256b6df7467a8f9fdd5581c
luffynando <luffynando@gmail.com>
2019-01-23 20:08:25 -0600

    SCP Login add - Init

--- include/staff/login.header.php
+++ include/staff/login.header.php
@@ -5,21 +5,44 @@ header("X-Frame-Options: SAMEORIGIN");
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="refresh" content="7200" />
     <title>osTicket :: <?php echo __('Agent Login'); ?></title>
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/adminlte/components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/adminlte/components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/adminlte/components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/adminlte/css/AdminLTE.min.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/adminlte/fonts/font.css">
+
     <link rel="stylesheet" href="css/login.css" type="text/css" />
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome.min.css?035fd0a"/>
     <meta name="robots" content="noindex" />
     <meta http-equiv="cache-control" content="no-cache" />
     <meta http-equiv="pragma" content="no-cache" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-1.11.2.min.js?035fd0a"></script>
+    <!-- jQuery 3 -->
+    <script src="<?php echo ROOT_PATH; ?>assets/adminlte/components/jquery/dist/jquery.min.js"></script>
+    <!-- jQueryMigrate -->
+    <script src="<?php echo ROOT_PATH; ?>assets/adminlte/js/jquery-migrate-3.0.1.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             $("input:not(.dp):visible:enabled:first").focus();
          });
     </script>
 </head>
-<body id="loginBody">
+<body class="hold-transition login-page brickwall">
+<div class="login-box">
 
--- include/staff/login.tpl.php
+++ include/staff/login.tpl.php
@@ -2,53 +2,55 @@
 include_once(INCLUDE_DIR.'staff/login.header.php');
 $info = ($_POST && $errors)?Format::htmlchars($_POST):array();
 ?>
-<div id="brickwall"></div>
-<div id="loginBox">
-    <div id="blur">
-        <div id="background"></div>
-    </div>
+  <div class="login-logo">
     <h1 id="logo"><a href="index.php">
         <span class="valign-helper"></span>
         <img src="logo.php?login" alt="osTicket :: <?php echo __('Staff Control Panel');?>" />
     </a></h1>
-    <h3><?php echo Format::htmlchars($msg); ?></h3>
-    <div class="banner"><small><?php echo ($content) ? Format::display($content->getLocalBody()) : ''; ?></small></div>
+  </div>
+  <div class="login-box-body">
+    <p class="login-box-msg"><h3><?php echo Format::htmlchars($msg); ?></h3>
+    <div class="banner"><small><?php echo ($content) ? Format::display($content->getLocalBody()) : ''; ?></small></div></p>
     <form action="login.php" method="post" id="login">
         <?php csrf_token(); ?>
         <input type="hidden" name="do" value="scplogin">
-        <fieldset>
-        <input type="text" name="userid" id="name" value="<?php
+        <div class="form-group has-feedback">
+            <input class="form-control" type="text" name="userid" id="name" value="<?php
             echo $info['userid']; ?>" placeholder="<?php echo __('Email or Username'); ?>"
             autofocus autocorrect="off" autocapitalize="off">
-        <input type="password" name="passwd" id="pass" placeholder="<?php echo __('Password'); ?>" autocorrect="off" autocapitalize="off">
-            <?php if ($show_reset && $cfg->allowPasswordReset()) { ?>
-            <h3 style="display:inline"><a href="pwreset.php"><?php echo __('Forgot My Password'); ?></a></h3>
-            <?php } ?>
-            <button class="submit button pull-right" type="submit" name="submit"><i class="icon-signin"></i>
-                <?php echo __('Log In'); ?>
-            </button>
-        </fieldset>
-    </form>
-<?php
-$ext_bks = array();
-foreach (StaffAuthenticationBackend::allRegistered() as $bk)
-    if ($bk instanceof ExternalAuthentication)
-        $ext_bks[] = $bk;
-
-if (count($ext_bks)) { ?>
-<div class="or">
-    <hr/>
-</div><?php
-    foreach ($ext_bks as $bk) { ?>
-<div class="external-auth"><?php $bk->renderExternalLink(); ?></div><?php
-    }
-} ?>
-
-    <div id="company">
-        <div class="content">
-            <?php echo __('Copyright'); ?> &copy; <?php echo Format::htmlchars($ost->company) ?: date('Y'); ?>
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
-    </div>
+        <div class="form-group has-feedback">
+            <input class="form-control" type="password" name="passwd" id="pass" placeholder="<?php echo __('Password'); ?>" autocorrect="off" autocapitalize="off">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+            <button class="btn btn-primary btn-flat pull-right" type="submit" name="submit"><i class="icon-signin"></i>
+            <?php echo __('Log In'); ?>
+            </button>
+        </div>
+    </form>
+    <?php
+    $ext_bks = array();
+    foreach (StaffAuthenticationBackend::allRegistered() as $bk)
+        if ($bk instanceof ExternalAuthentication)
+            $ext_bks[] = $bk;
+    if (count($ext_bks)) { ?>
+        <div class="social-auth-links text-center">
+            <p>- OR -</p>
+            <?php
+            foreach ($ext_bks as $bk) { ?>
+                <div class="external-auth"><?php $bk->renderExternalLink(); ?></div><?php
+            } ?>
+        </div>
+        <?php
+    } ?>
+    <br/>
+    <?php if ($show_reset && $cfg->allowPasswordReset()) { ?>
+        <a href="pwreset.php"><?php echo __('Forgot My Password'); ?></a><br/>
+    <?php } ?>
+    <?php echo __('Copyright'); ?> &copy; <?php echo Format::htmlchars($ost->company) ?: date('Y'); ?>
+  </div>
 </div>
 <div id="poweredBy"><?php echo __('Powered by'); ?>
     <a href="http://www.osticket.com" target="_blank">
@@ -67,5 +69,7 @@ if (count($ext_bks)) { ?>
         #loginBox:after { background-color: white !important; }
     </style>
     <![endif]-->
+    <!-- Bootstrap 3.3.7 -->
+    <script src="<?php echo ROOT_PATH; ?>assets/adminlte/components/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
